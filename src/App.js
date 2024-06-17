32b54e1fd1c9c633598df7a11eaf03f6026bc8e1
sangam shinde <sangampshinde@gmail.com>
2024-06-17 11:45:59 +0530

    folder create

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-// File and Folder structure
+// design for add to cart
 
-/*
+import React from 'react';
+import HomeContainer from './Container/HomeContainer';
+import './App.css';
 
-components
-containers
-service
-      actions
-
-      reducers
-      
-      constants
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-
-
-import React from 'react'
 
 function App() {
   return (
     <div>
+      <HomeContainer/>
+    
       
     </div>
   )
 }
 
 export default App
+
+
+
+// =============================================================================================================================
+// // File and Folder structure
+
+// /*
+
+// components->--
+//    to        |
+// containers-<--
+// service
+//       actions
+
+//       reducers
+      
+//       constants
+
+// */
+
+
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default App
 
 // =========================================================================================================================
 // // Props revisions for redux #4
---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Component</h1>
+      <div className='cart-wrapper'>
+        <div className='img-wrapper item'> 
+          <img src='' alt=''/>
+        </div>
+        <div className='text-wrapper item'> 
+          <span>I-Phone</span>
+          <span>Price:$1000.00</span>
+        </div>
+        <div className='btn-wrapper item'> 
+         <button>Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
