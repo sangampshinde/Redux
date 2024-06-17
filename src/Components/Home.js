772602ce366964f3c9f82125cac9713a423b82a1
sangam shinde <sangampshinde@gmail.com>
2024-06-17 12:15:42 +0530

    cart redux design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,16 +5,20 @@ function Home() {
     <div>
       <h1>Home Component</h1>
       <div className='cart-wrapper'>
+
         <div className='img-wrapper item'> 
-          <img src='' alt=''/>
+          <img src='iphone.jpg' alt=''/>
         </div>
+
         <div className='text-wrapper item'> 
-          <span>I-Phone</span>
+          <span>I-Phone</span><br/>
           <span>Price:$1000.00</span>
         </div>
+
         <div className='btn-wrapper item'> 
          <button>Add To Cart</button>
         </div>
+
       </div>
     </div>
   )
